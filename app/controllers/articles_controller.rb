7421c8f78e4cc61ed9d3408030a3fb0d205714c2
David Murillo <dmz866@hotmail.com>
2018-01-16 03:02:09 +0000

    Added error section, redirect to show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,26 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def index
+    
+  end
+  
   def create
     # This code will display: {"title"=>"as", "description"=>"asads"}
     #render plain: params[:article].inspect
     
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if(@article.save)
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+      #redirect_to action: "show", id: 5
+    else
+      render 'new' # GO TO NEW TEMPLATE AGAIN
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
