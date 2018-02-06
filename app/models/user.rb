36b5c9d4c2fd9d6ff2423d9b4432e350c7b8d49b
David Murillo <dmz866@hotmail.com>
2018-02-06 03:00:43 +0000

    Admin functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase} ## TURN EMAIL DOWNCASE BEFORE SAVING IN DB
   validates :username, presence: true, length: {minimum: 4, maximum: 25}, 
   uniqueness: {case_sensitive: false}
